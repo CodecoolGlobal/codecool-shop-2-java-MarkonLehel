9089b8e16e5d5a1ec973abf57f0aacaae090fb09
MarkonLehel <lehel.markon@gmail.com>
2021-06-14 15:00:20 +0200

    Added getAllCartItems_CorrectReturn_ReturnsTrue and getTotalCartPrice_CorrectPrice_ReturnsTrue test

--- src/test/Java/services/CartServiceTest.java
+++ src/test/Java/services/CartServiceTest.java
@@ -1,9 +1,13 @@
 package services;
 
 import com.codecool.shop.model.Cart;
+import com.codecool.shop.model.CartItem;
 import com.codecool.shop.service.CartService;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,17 +18,26 @@ class CartServiceTest {
 
     Cart mockCart = mock(Cart.class);
     CartService cartService = new CartService(mockCart);
+    CartItem mockItem1 = mock(CartItem.class);
+    CartItem mockItem2 = mock(CartItem.class);
+    CartItem mockItem3 = mock(CartItem.class);
+
+
+    List<CartItem> mockCartItems = Arrays.asList(mockItem1,mockItem2,mockItem3);
 
 
 
     @Test
-    void getAllCartItems() {
-
+    void getAllCartItems_CorrectReturn_ReturnsTrue() {
+    when(mockCart.getCartItems()).thenReturn(mockCartItems);
+    assertEquals(mockCartItems,cartService.getAllCartItems());
 
     }
 
     @Test
-    void getTotalCartPrice() {
+    void getTotalCartPrice_CorrectPrice_ReturnsTrue() {
+        when(mockCart.getTotalPrice()).thenReturn(500F);
+        assertEquals(500F, cartService.getTotalCartPrice());
     }
 
     @Test
