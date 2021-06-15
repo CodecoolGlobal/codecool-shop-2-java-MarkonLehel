19037e7bf74e5f8eac1a46e813564fd52fe84b4f
Mate Magyar <mate.m.magyar@gmail.com>
2021-06-15 12:00:36 +0200

    Add SupplierServiceTest imports and mockSupplierDao in @BeforeEach

--- src/test/Java/services/SupplierServiceTest.java
+++ src/test/Java/services/SupplierServiceTest.java
@@ -1,10 +1,19 @@
 package services;
 
+import com.codecool.shop.dao.SupplierDao;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 class SupplierServiceTest {
+    SupplierDao mockSupplierDao;
+
+    @BeforeEach
+    void beforeEach() {
+        mockSupplierDao = mock(SupplierDao.class);
+    }
 
     @Test
     void getSupplier() {
