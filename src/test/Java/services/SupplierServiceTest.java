779b6e8a7a85ccd8f2a82c2bccde6cc272e0e5ad
Mate Magyar <mate.m.magyar@gmail.com>
2021-06-15 12:28:01 +0200

    Add getSupplier_findExistingSupplier_returnTrue test for SupplierServiceTest

--- src/test/Java/services/SupplierServiceTest.java
+++ src/test/Java/services/SupplierServiceTest.java
@@ -1,22 +1,31 @@
 package services;
 
 import com.codecool.shop.dao.SupplierDao;
+import com.codecool.shop.model.Supplier;
+import com.codecool.shop.service.SupplierService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class SupplierServiceTest {
     SupplierDao mockSupplierDao;
+    Supplier mockSupplier;
+    SupplierService supplierService;
 
     @BeforeEach
     void beforeEach() {
         mockSupplierDao = mock(SupplierDao.class);
+        mockSupplier = mock(Supplier.class);
+        supplierService = new SupplierService(mockSupplierDao);
     }
 
     @Test
-    void getSupplier() {
+    void getSupplier_findExistingSupplier_returnTrue() {
+        when(mockSupplierDao.find(1)).thenReturn(mockSupplier);
+        assertEquals(mockSupplier, supplierService.getSupplier(1));
     }
 
     @Test
