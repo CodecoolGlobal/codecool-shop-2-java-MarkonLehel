35bbf9f16bca24d7bbc506c8fc5ba1b8484ada82
KT-GitHub-code <krakovszkyt@gmail.com>
2021-06-15 09:45:58 +0200

    add ProductServiceTest

---
+++ src/test/Java/services/ProductServiceTest.java
@@ -0,0 +1,22 @@
+package services;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    public void getProduct_ValidID_ReturnTrue(){
+
+    }
+
+}
